c560abadd33472ced6eaf1afe9885c63dd618d9c
Tejas Shah <61719799+Tshah-95@users.noreply.github.com>
2024-03-16 18:01:42 -0400

    main screen basics

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,54 @@
+"use client";
+
 import Image from "next/image";
+import Lottie from "lottie-react";
+import smileyBubbles from "../public/smiley-bubbles.json";
+import { easeIn, easeInOut, motion } from "framer-motion";
+
+const scaleAnimation = {
+  scale: [1, 1.1, 1],
+  transition: {
+    duration: 3,
+    ease: "easeInOut",
+    repeat: Infinity,
+  },
+};
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-lg font-bold">Welcome to Next.js!</h1>
-      <p className="text-md">
-        Get started by editing <code>pages/index.js</code>
-      </p>
-      <div className="flex flex-col items-center">
-        <Image src="/vercel.svg" alt="Vercel Logo" width={300} height={100} />
-        <Image src="/next.svg" alt="Next.js Logo" width={300} height={100} />
+    <main className="flex h-screen justify-center items-center bg-primary">
+      <div className="flex flex-col h-full items-center justify-center max-w-screen-sm p-10">
+        <motion.div
+          className="flex justify-center items-center"
+          animate={scaleAnimation}
+        >
+          <div className="h-full w-[90%] max-w-[450px] max-h-full relative justify-center items-center">
+            <Image
+              alt="Buzz Life Games Title"
+              src="/title.png"
+              width={400}
+              height={400}
+              priority
+            />
+          </div>
+        </motion.div>
+        <div className="flex flex-col relative bottom-[10%]">
+          <div className="flex justify-center">
+            <div className="w-[80%] max-w-[450px]">
+              <Lottie animationData={smileyBubbles} loop />
+            </div>
+          </div>
+          <div className="flex justify-center items-center">
+            <motion.div
+              className="bg-tertiary rounded-lg shadow-md"
+              animate={scaleAnimation}
+            >
+              <button className="text-[white] font-bold text-4xl py-5 px-12">
+                Play
+              </button>
+            </motion.div>
+          </div>
+        </div>
       </div>
     </main>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,12 +7,12 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    colors: {
+      primary: "#ccccff",
+      secondary: "#E32565",
+      tertiary: "#3B99FB",
+      lightBlue: "#33DDFF",
+      lightBlack: "#121212",
     },
   },
   plugins: [],
