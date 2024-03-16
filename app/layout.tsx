cd49165d6307e72d9e3d333ef7c517dd6c91dc93
Tejas Shah <61719799+Tshah-95@users.noreply.github.com>
2024-03-16 18:46:13 -0400

    refactor layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import Image from "next/image";
 
 const chaloops = localFont({
   src: [
@@ -34,7 +35,23 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={chaloops.className}>{children}</body>
+      <body
+        className={chaloops.className.concat(
+          " max-h-screen w-full overflow-hidden"
+        )}
+      >
+        <main className="flex h-screen max-h-screen justify-center items-center bg-primary">
+          <div className="absolute top-[50%] w-[135%] max-w-screen-md aspect-[6/5.5]">
+            <Image
+              alt="An image of a beer with foam in the background"
+              src="/beer-with-foam.png"
+              fill
+              priority
+            />
+          </div>
+          {children}
+        </main>
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -16,40 +16,38 @@ const scaleAnimation = {
 
 export default function Home() {
   return (
-    <main className="flex h-screen justify-center items-center bg-primary">
-      <div className="flex flex-col h-full items-center justify-center max-w-screen-sm p-10">
-        <motion.div
-          className="flex justify-center items-center"
-          animate={scaleAnimation}
-        >
-          <div className="h-full w-[90%] max-w-[450px] max-h-full relative justify-center items-center">
-            <Image
-              alt="Buzz Life Games Title"
-              src="/title.png"
-              width={400}
-              height={400}
-              priority
-            />
-          </div>
-        </motion.div>
-        <div className="flex flex-col relative bottom-[10%]">
-          <div className="flex justify-center">
-            <div className="w-[80%] max-w-[450px]">
-              <Lottie animationData={smileyBubbles} loop />
-            </div>
-          </div>
-          <div className="flex justify-center items-center">
-            <motion.div
-              className="bg-tertiary rounded-lg shadow-md"
-              animate={scaleAnimation}
-            >
-              <button className="text-[white] font-bold text-4xl py-5 px-12">
-                Play
-              </button>
-            </motion.div>
+    <div className="flex flex-col h-full items-center justify-center max-w-screen-sm p-10">
+      <motion.div
+        className="flex justify-center items-center"
+        animate={scaleAnimation}
+      >
+        <div className="h-full w-[90%] max-w-[450px] max-h-full relative justify-center items-center">
+          <Image
+            alt="Buzz Life Games Title Image"
+            src="/title.png"
+            width={400}
+            height={400}
+            priority
+          />
+        </div>
+      </motion.div>
+      <div className="flex flex-col relative bottom-[10%]">
+        <div className="flex justify-center">
+          <div className="w-[80%] max-w-[450px] min-h-[200px]">
+            <Lottie animationData={smileyBubbles} loop />
           </div>
         </div>
+        <div className="flex justify-center items-center">
+          <motion.div
+            className="bg-tertiary rounded-lg shadow-md"
+            animate={scaleAnimation}
+          >
+            <button className="text-[white] font-bold text-4xl py-5 px-12">
+              Play
+            </button>
+          </motion.div>
+        </div>
       </div>
-    </main>
+    </div>
   );
 }
